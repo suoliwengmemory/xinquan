00066aeac4c36ed237191741f09a6c70e3dcf11e
suoliwengmemory <33852808+suoliwengmemory@users.noreply.github.com>
2017-12-16 09:34:39 +0800

    Add files via upload

--- Lesson/views.py
+++ Lesson/views.py
@@ -2,14 +2,21 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Lesson, Theme
 from Goods.models import Kind
+from Base.models import Side, Bow, Tabbar
 
 def lesson(request):
     all_Lessons = Lesson.objects.all()
     all_Themes = Theme.objects.all()
     all_Kinds = Kind.objects.all()
+    all_Bows = Bow.objects.all()
+    all_Sides = Side.objects.all()
+    all_Tabbars = Tabbar.objects.all()
     context = {
         "all_Themes": all_Themes,
         "all_Lessons": all_Lessons,
+        "all_Bows": all_Bows,
+        "all_Sides": all_Sides,
+        "all_Tabbars": all_Tabbars,
         "all_Kinds": all_Kinds
     }
     return render(request, "lesson.html", context)
